237186a3c3037f5ed308e4fd2fbee51c9e24e5fb
Feng Qiang <f.qiang@gmail.com>
2014-10-14 18:46:31 +0100

    modified for compile in mac

--- autodiff_library_test/Release/src/subdir.mk
+++ autodiff_library_test/Release/src/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"/Users/s0965328/workspace/autodiff_library/autodiff_library/src" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
